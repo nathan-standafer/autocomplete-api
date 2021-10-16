6efe2a5d7d0e2b2d04f18867213804b28e1a8e3f
Nathan Standafer <standafer_na@yahoo.com>
2021-10-15 21:46:26 -0400

    Added a test.

--- src/test/java/com/nas/autocomplete/AutocompleteApplicationTests.java
+++ src/test/java/com/nas/autocomplete/AutocompleteApplicationTests.java
@@ -1,13 +1,36 @@
 package com.nas.autocomplete;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import com.nas.autocomplete.restservice.AutocompleteTitleController;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class AutocompleteApplicationTests {
 
+    @LocalServerPort
+    private int port;
+    
+    @Autowired
+    private AutocompleteTitleController controller;
+    
+    @Autowired
+    private TestRestTemplate restTemplate;
+    
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+	
+   @Test
+    public void searchUsingWildcard() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/autocomplete&searchString=Arriving", String.class)).contains("Arriving");
+    }
 
 }
